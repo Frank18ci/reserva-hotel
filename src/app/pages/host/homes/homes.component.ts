5f5a7d9826c97e350634b1ed5813d4a1aca87c8c
Frank <fcarpio113@gmail.com>
2024-12-15 20:57:45 -0500

    Mejoras pagina host

--- src/app/pages/host/homes/homes.component.ts
+++ src/app/pages/host/homes/homes.component.ts
@@ -8,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './homes.component.css'
 })
 export class HomesComponent {
-
+  onChildClick(event : MouseEvent){
+    const padre = (event.currentTarget as HTMLElement).parentElement;
+    if(parent){
+      const child2 = padre?.parentElement?.querySelector('.acordion-body')
+      if(child2){
+        const iconAd = padre?.querySelector('.container-icon-svg-ad')?.querySelector('svg')
+        if(iconAd){
+          if(child2.className.includes('d-none') ){
+            iconAd.style.transform = 'rotate(180deg)';
+            child2.classList.remove('d-none')
+            child2.classList.add('d-block')
+          } else {
+            iconAd.style.transform = 'rotate(0deg)';
+            child2.classList.add('d-none')
+            child2.classList.remove('d-block')
+          }
+        }
+        
+        
+        
+      }
+    }
+  }
 }
